34a741bd4118eb4fa350a5e2f06fa180997bfec3
Brady Herder <bradyherder@hotmail.com>
2016-02-07 07:44:17 -0800

    Signed-off-by: Brady Herder <bradyherder@hotmail.com>

---
+++ .ino.cpp
@@ -0,0 +1,13 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2016-02-07 07:43:40
+
+#include "Arduino.h"
+#include <SPI.h>
+#include "RF24.h"
+void setup() ;
+void loop() ;
+
+#include "NRF24L01.ino"
+
---
+++ Release/.ino.cpp.d
@@ -0,0 +1,46 @@
+.ino.cpp.o: ../.ino.cpp \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ E:\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ E:\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ E:\Arduino\hardware\arduino\avr\libraries\SPI/SPI.h \
+ C:\Users\Brady\Documents\Arduino\libraries\RF24-master/RF24.h \
+ C:\Users\Brady\Documents\Arduino\libraries\RF24-master/RF24_config.h \
+ ../NRF24L01.ino
+
+E:\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h:
+
+E:\Arduino\hardware\arduino\avr\cores\arduino/binary.h:
+
+E:\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h:
+
+E:\Arduino\hardware\arduino\avr\cores\arduino/WString.h:
+
+E:\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h:
+
+E:\Arduino\hardware\arduino\avr\cores\arduino/Stream.h:
+
+E:\Arduino\hardware\arduino\avr\cores\arduino/Print.h:
+
+E:\Arduino\hardware\arduino\avr\cores\arduino/Printable.h:
+
+E:\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h:
+
+E:\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h:
+
+E:\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h:
+
+E:\Arduino\hardware\arduino\avr\libraries\SPI/SPI.h:
+
+C:\Users\Brady\Documents\Arduino\libraries\RF24-master/RF24.h:
+
+C:\Users\Brady\Documents\Arduino\libraries\RF24-master/RF24_config.h:
+
+../NRF24L01.ino:
